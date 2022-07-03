a18798b133444db75b6d14ce4758956cc0bb3dce
Yang HyeonBin <90481975+YangHyeonBin@users.noreply.github.com>
2022-07-03 23:00:35 +0900

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import App from './App'; // .js라는 확장자명은 생략해야 함
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
